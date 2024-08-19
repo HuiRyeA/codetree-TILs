92bceefea0a4cdba2bb97e2abfbd06441cc4e605
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-08-19 20:59:21 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240819/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void pstar(){
+    cout << "**********" << endl;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int i{0}; i < 5; i++){
+        pstar();
+    }
+    return 0;
+}
