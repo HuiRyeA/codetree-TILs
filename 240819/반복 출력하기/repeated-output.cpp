78c69eef5148b2772032ba34ea64d9babf6e1d55
HuiRyeA <59016424+HuiRyeA@users.noreply.github.com>
2024-08-19 21:01:39 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240819/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void pline(int v){
+    for(int i{0}; i < v; i++){
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cyc;
+    cin >> cyc;
+    pline(cyc);
+    return 0;
+}
